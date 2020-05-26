adbb8f759fdfdc61357611f9ee09eeb3ab59c409
vagrant <vagrant@localhost.localdomain>
2020-05-26 21:50:07 +0000

    check port setting...

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to the homepage baby.');
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('What\'s up!');
 });
